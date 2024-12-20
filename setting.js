c0cae5dd07d85717316d8e2d6aca999ce1b46681
bagashzz <bagasathallahramadhann@gmail.com>
2024-12-20 07:07:47 +0700

    hai

---
+++ setting.js
@@ -0,0 +1,36 @@
+module.exports = {
+  name: "bagash",
+  version: "4.1.0",
+  number: "",
+  owner: {
+    name: "bagash",
+    number: "6285117112274",
+    whatsapp: "6285117112274@s.whatsapp.net",
+    instagram: "https://instagram.com/xyronesiaidreal",
+  },
+  author: {
+    name: "XyroHost",
+    number: "6285117112274",
+    whatsapp: "6285117112274@s.whatsapp.net",
+    instagram: "https://instagram.com/thomvelz_real",
+  },
+  features: {
+    antiCall: {
+      status: true,
+      block: false,
+    },
+    selfMode: true,
+    broadcast: {
+      text: "",
+      limit: 9999,
+      jeda: 7000,
+      filterContact: false,
+    },
+    pushContacts: {
+      text: "",
+      limit: 9999,
+      jeda: 7000,
+      filterContacts: false,
+    },
+  },
+};
